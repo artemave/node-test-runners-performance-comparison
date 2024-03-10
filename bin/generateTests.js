ecfb49005a1da7205a3cb7a74aadd2062cee73d8
artemave <artemave@gmail.com>
2024-03-10 15:12:02 +0100

    Update everything and add a bunch more test runners

--- bin/generateTests.js
+++ bin/generateTests.js
@@ -1,32 +1,35 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const glob = require('glob')
+import fs from 'fs'
+import * as glob from 'glob'
 
 if (fs.existsSync('./build')) {
-  fs.rmdirSync('./build', {recursive: true})
+  fs.rmSync('./build', {recursive: true})
 }
-fs.mkdirSync('./build/donc', {recursive: true})
+fs.mkdirSync('./build/ars', {recursive: true})
 fs.mkdirSync('./build/mocha', {recursive: true})
 fs.mkdirSync('./build/jest', {recursive: true})
-fs.mkdirSync('./build/banana-shark', {recursive: true})
 fs.mkdirSync('./build/tape', {recursive: true})
 fs.mkdirSync('./build/uvu', {recursive: true})
+fs.mkdirSync('./build/node', {recursive: true})
+fs.mkdirSync('./build/ava', {recursive: true})
+fs.mkdirSync('./build/vitest', {recursive: true})
 
-glob.sync('./node_modules/sails/lib/**/*.js').forEach(path => {
+glob.sync('./node_modules/sails/lib/**/*.js').forEach(p => {
+  const path = p.replace('node_modules/', '')
   const fileName = path.split('/').pop()
   const testFileName = fileName.replace('.js', 'Test.js')
 
-  const doncTestFileContents = `require('../.${path}')
-  const {test} = require('donc')
-  const assert = require('assert')
+  const arsTestFileContents = `import '${path}'
+  import {test} from 'assert-raisins'
+  import assert from 'assert'
 
   test('${path} works', () => {
     assert.ok(true)
   })`
 
-  const mochaTestFileContents = `require('../.${path}')
-  const assert = require('assert')
+  const mochaTestFileContents = `import '${path}'
+  import assert from 'assert'
 
   describe('${path}', function() {
     it('works', function() {
@@ -34,24 +37,14 @@ glob.sync('./node_modules/sails/lib/**/*.js').forEach(path => {
     })
   })`
 
-  const jestTestFileContents = `require('../.${path}')
+  const jestTestFileContents = `import '${path}'
 
   test('${path} works', () => {
     expect(1).toBe(1)
   })`
 
-  const bsTestFileContents = `require('../.${path}')
-
-  module.exports = describe => {
-    describe(
-      'one hundred and twenty three',
-      () => 123,
-      it => it.equals(123)
-    )
-  }`
-
-  const tapeTestFileContents = `require('../.${path}')
-  const test = require('tape')
+  const tapeTestFileContents = `import '${path}'
+  import test from 'tape'
 
   test('${path}', function (t) {
     t.plan(1);
@@ -59,9 +52,9 @@ glob.sync('./node_modules/sails/lib/**/*.js').forEach(path => {
     t.equal(1, 1);
   })`
 
-  const uvuTestFileContents = `require('../.${path}')
-  const { test } = require('uvu')
-  const assert = require('uvu/assert')
+  const uvuTestFileContents = `import '${path}'
+  import { test } from 'uvu'
+  import * as assert from 'uvu/assert'
 
   test('${path}', function () {
     assert.is(1, 1)
@@ -69,10 +62,34 @@ glob.sync('./node_modules/sails/lib/**/*.js').forEach(path => {
 
   test.run()`
 
-  fs.writeFileSync(`./build/donc/${testFileName}`, doncTestFileContents)
+  const nodeTestFileContents = `import '${path}'
+  import { it } from 'node:test'
+  import assert from 'node:assert'
+
+  it('${path} works', function() {
+    assert.ok(true)
+  })`
+
+  const avaTestFileContents = `import '${path}'
+  import test from 'ava'
+
+  test('${path} works', t => {
+    t.pass()
+  })`
+
+  const vitestTestFileContents = `import '${path}'
+  import { expect, test } from 'vitest'
+
+  test('${path} works', () => {
+    expect(1).toBe(1)
+  })`
+
+  fs.writeFileSync(`./build/ars/${testFileName}`, arsTestFileContents)
   fs.writeFileSync(`./build/mocha/${testFileName}`, mochaTestFileContents)
   fs.writeFileSync(`./build/jest/${testFileName}`, jestTestFileContents)
-  fs.writeFileSync(`./build/banana-shark/${testFileName}`, bsTestFileContents)
   fs.writeFileSync(`./build/tape/${testFileName}`, tapeTestFileContents)
   fs.writeFileSync(`./build/uvu/${testFileName}`, uvuTestFileContents)
+  fs.writeFileSync(`./build/node/${testFileName}`, nodeTestFileContents)
+  fs.writeFileSync(`./build/ava/${testFileName}`, avaTestFileContents)
+  fs.writeFileSync(`./build/vitest/${testFileName}`, vitestTestFileContents)
 })
--- readme.md
+++ readme.md
@@ -1,30 +1,32 @@
 # Node test runners performance comparison
 
-Not an exhaustive comparison by any means (although more test runners can be added easily).
+Why does this exist? I was experimenting with a test runner that requires test files "statically" (via `node --require`) and wanted to see if this approach improves test start up time. I wanted to compare the aformentioned test runned - [assert-raisins](https://github.com/artemave/assert-raisins) - with [mocha](https://github.com/mochajs/mocha), because mocha (just like any other test runner I looked into) requires test files "dynamically", iterating over glob results.
 
-Why does this exist? I was experimenting with a test runner that requires test files "statically" (via `node -r`) and wanted to see if this approach improves test start up time. I wanted to compare the aformentioned test runned - [donc](https://github.com/artemave/donc) - with [mocha](https://github.com/mochajs/mocha), because mocha (just like any other test runner I looked into) requires test files "dynamically", iterating over glob results.
+Once test framework was in place, I added a bunch more popular test runners into the mix.
 
-To generate sizable load, I took the largest Node project I could think of - [Sails](https://sailsjs.com/) - and for each `.js` file in `lib` I generated a dumb test file (in each contending tech) that requires that Sails file.
-
-Once the "framework" was in place, it was easy to add more test runners. And so in went an [intriguing novelty](https://github.com/featurist/banana-shark), a  [trusty workhorse](https://github.com/substack/tape) and an [unusably slow thing that's nonetheless used by so many](https://jestjs.io/).
+To generate sizable "import" load, I took a large Node project - [Sails](https://sailsjs.com/) - and for each `.js` file in `lib` I generated a dumb test file (in each contending tech) that requires that Sails file.
 
 Results on my Linux machine (seconds):
 
 | runner | real | user | system |
 | ------ | ----:| ----:| ------:|
-|Jest all tests|5.36|31.77|2.57|
-|Jest single test|2.07|2.29|0.24|
-|Mocha all tests|0.47|0.52|0.03|
-|Mocha single test|0.37|0.41|0.03|
-|Uvu all tests|0.44|0.51|0.04|
-|Uvu single test|0.25|0.27|0.02|
-|Tape all tests|0.42|0.48|0.02|
-|Tape single test|0.29|0.31|0.02|
-|Donc all tests|0.41|0.41|0.06|
-|Donc single test|0.33|0.32|0.05|
-|Banana-shark all tests|0.30|0.32|0.02|
-|Banana-shark single test|0.24|0.23|0.04|
-|Node start up time|0.03|0.03|0.00|
+|Node all tests|1.03|10.57|2.69|
+|Node single test|0.20|0.19|0.04|
+|Jest all tests|2.83|31.60|4.69|
+|Jest single test|0.58|0.61|0.13|
+|Mocha all tests|0.33|0.32|0.08|
+|Mocha single test|0.28|0.30|0.05|
+|Tape all tests|0.32|0.32|0.08|
+|Tape single test|0.20|0.21|0.05|
+|Vitest all tests|1.26|7.81|2.06|
+|Vitest single test|0.90|3.43|1.43|
+|Ava all tests|3.32|35.41|5.47|
+|Ava single test|0.47|0.50|0.10|
+|Uvu all tests|0.26|0.26|0.06|
+|Uvu single test|0.15|0.15|0.03|
+|Assert-raisins all tests|0.35|0.34|0.09|
+|Assert-raisins single test|0.24|0.23|0.05|
+|Node start up time|0.01|0.01|0.00|
 
 ## Usage
 
